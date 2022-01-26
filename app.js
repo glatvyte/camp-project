c10ed2c875f71e0b521b67793c6da4755a385e5d
Gintare <gintare@Gintares-MacBook-Air.local>
2022-01-26 16:08:20 +0200

    add start script

--- app.js
+++ app.js
@@ -161,8 +161,9 @@ app.use((err, req, res, next) => {
     res.status(statusCode).render('error', { err })
 })
 
-app.listen(3000, () => {
-    console.log('Serving on port 3000')
+const port = process.env.PORT || 3000; //indicate port of Heroku
+app.listen(port, () => {
+    console.log(`Serving on port ${port}`)
 })
 
 
